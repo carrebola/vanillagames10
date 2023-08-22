17dce1c387d5763dde29cde9173fa17841e0c91d
carrebola <carrebola@fpllefia.com>
2023-08-22 11:20:56 +0200

    Configura ESLint

--- src/main.js
+++ src/main.js
@@ -6,18 +6,17 @@ import './scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
-//Importamos la vista por defecto (que será home)
-async function cargarVista(){
+// Importamos la vista por defecto (que será home)
+async function cargarVista () {
   const componenteHome = await import('./vistas/homeVista')
   const homeVista = componenteHome.default
-  //Inyectamos la vista home
+  // Inyectamos la vista home
   document.querySelector('main').innerHTML = homeVista.template
 }
 cargarVista()
 
-//Inyectamos el componente header
+// Inyectamos el componente header
 document.querySelector('header').innerHTML = header.template
 
-//Inyectamos el componente footer
+// Inyectamos el componente footer
 document.querySelector('footer').innerHTML = footer.template
-
