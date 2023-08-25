665684585f0339a2b3893b4f5bc4753fa04a0e4b
carrebola <carrebola@fpllefia.com>
2023-08-25 21:26:00 +0200

    .

--- src/main.js
+++ src/main.js
@@ -1,21 +1,15 @@
 import { header } from './componentes/header'
 import { footer } from './componentes/footer'
+import { enrutador } from './componentes/enrutador'
 
 import './scss/styles.scss'
-
-// Importamos la vista por defecto (que será home)
-async function cargarVista () {
-  const componente = await import('./vistas/registroVista')
-  const vista = componente.default
-  // Inyectamos la vista home
-  document.querySelector('main').innerHTML = vista.template
-  // Ejecutamos la lógica de la vista
-  vista.script()
-}
-cargarVista()
 
 // Inyectamos el componente header
 document.querySelector('header').innerHTML = header.template
 
 // Inyectamos el componente footer
 document.querySelector('footer').innerHTML = footer.template
+
+enrutador.observadorRutas()
+// Cargamos la página home
+window.location = '#/home'
